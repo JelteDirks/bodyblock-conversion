cd1dd406b1023939fbd8b7cff0537a1d51bdc9ad
Jelte Dirks <j.dirks@outlook.com>
2020-10-09 12:21:23 +0200

    intermittent: trying to find the offset for labels

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {Regel} from "./Regel";
 
 const testfilepath = path.resolve('test/test_fiddle');
 const regels: Regel[]= [];
+const counterLineRE = /[0-9]{15,}/;
 
 (async function () {
     const filestream = fs.createReadStream(testfilepath, {
@@ -18,7 +19,9 @@ const regels: Regel[]= [];
     });
 
     for await (const line of rlInterface) {
-
+        if (counterLineRE.test(line)) {
+            console.log(line);
+        }
         processLine(line, regels);
     }
 
