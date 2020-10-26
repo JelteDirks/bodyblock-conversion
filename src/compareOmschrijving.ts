3b9c8342b8964387aa11e6554a7f4e5454662452
Jelte Dirks <j.dirks@outlook.com>
2020-10-26 14:31:20 +0100

    comparing omschrijving simplified

--- src/compareOmschrijving.ts
+++ src/compareOmschrijving.ts
@@ -1,15 +1,5 @@
 import {toLowerCase} from "./toLowerCase";
 
-const spacesOnly = /^\s*$/
-const testre = /.*ansprakelijkheid.*/;
-
 export function compareOmschrijving(a: string, b: string): boolean {
-
-    console.log('comparing', a, b);
-
-    if (spacesOnly.test(a) && spacesOnly.test(b)) return true;
-
-    if (toLowerCase(a).trim() === toLowerCase(b).trim()) return true;
-
-    return false;
+    return toLowerCase(a).trim() === toLowerCase(b).trim();
 }
