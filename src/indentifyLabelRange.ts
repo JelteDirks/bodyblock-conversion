1cb658ae5d8a09016f71ddf5fd1e0a0843c2e2d2
Jelte Dirks <j.dirks@outlook.com>
2021-01-05 17:01:42 +0100

    identifying label ranges with procedure
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/indentifyLabelRange.ts
+++ src/indentifyLabelRange.ts
@@ -1,5 +1,44 @@
-import {Range} from "xlsx";
+import {CellAddress, CellObject, Range, Sheet} from "xlsx";
+import {findCellAddress} from "./findCellAddress";
+import {cellInRange} from "./cellInRange";
 
-export function identifyLabelRange(startRow: number, columnName:string = 'omschrijving'): Range | undefined {
-    return;
+const xlsx = require('xlsx');
+
+export function identifyLabelRange(sheetObj: Sheet, startRow: number, columnName: string = 'omschrijving'): Range | undefined {
+    const titleAddress: string[] = findCellAddress(sheetObj, columnName, xlsx.utils.decode_range('A1:I1'));
+
+    if (titleAddress.length === 0) {
+        throw 'no title address found for identifying label range';
+    }
+
+    const first: string = <string>titleAddress.pop();
+
+    const c: number = xlsx.utils.decode_cell(first).c;
+
+    let r: number = startRow;
+    let value: string | undefined = '';
+
+    const initialCellAddress: CellAddress = {r, c};
+    const initialCellObject: CellObject = sheetObj[xlsx.utils.encode_cell(initialCellAddress)];
+    const initialValue = initialCellObject.w;
+    const maxColumn: number = xlsx.utils.decode_range(sheetObj["!ref"]).e.c;
+
+    r = r + 1;
+
+    while (!value && (value !== initialValue)) {
+        const cellAddress: CellAddress = {r, c};
+        const cellObject: CellObject = sheetObj[xlsx.utils.encode_cell(cellAddress)];
+
+        if (!!cellObject) {
+            value = String(cellObject.v);
+        }
+
+        if (!cellInRange(cellAddress, xlsx.utils.decode_range(sheetObj["!ref"]))) {
+            return {s: {c, r: startRow}, e: {c: maxColumn, r: r - 1}};
+        }
+
+        r = r + 1;
+    }
+
+    return {s: {c, r: startRow}, e: {c: maxColumn, r: r - 2}};
 }
--- test/identifyLabelRange.spec.ts
+++ test/identifyLabelRange.spec.ts
@@ -1,4 +1,33 @@
+import {Sheet} from "xlsx";
+import {identifyLabelRange} from "../src/indentifyLabelRange";
+
+const xlsx = require('xlsx');
+
 describe('identifying label range', () => {
 
+    let sheetObj: Sheet;
 
+    beforeAll(() => {
+        sheetObj = require('./static/dummysheet.json');
+    });
+
+    test('first label', () => {
+        expect(identifyLabelRange(sheetObj, 1, 'omschrijving'))
+            .toStrictEqual(xlsx.utils.decode_range('A2:I3'));
+    });
+
+    test('first label no column name', () => {
+        expect(identifyLabelRange(sheetObj, 1))
+            .toStrictEqual(xlsx.utils.decode_range('A2:I3'));
+    });
+
+    test('single line label no column name', () => {
+        expect(identifyLabelRange(sheetObj, 33))
+            .toStrictEqual(xlsx.utils.decode_range('A34:I34'));
+    });
+
+    test('last line no column name', () => {
+        expect(identifyLabelRange(sheetObj, 35))
+            .toStrictEqual(xlsx.utils.decode_range('A36:I36'));
+    });
 });
