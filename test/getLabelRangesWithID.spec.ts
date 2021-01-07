77f972e50b39c98fca8873b28e109f67466d0c2d
Jelte Dirks <j.dirks@outlook.com>
2021-01-07 14:47:55 +0100

    more test cases
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- test/getLabelRangesWithID.spec.ts
+++ test/getLabelRangesWithID.spec.ts
@@ -19,4 +19,32 @@ describe('get ranges with id from excel file', () => {
             .toStrictEqual(xlsx.utils.decode_range('A2:I3'));
         expect(ranges[0].id).toBe('Soort verzekering');
     });
+
+    test('middle range', () => {
+        expect(ranges[5].range)
+            .toStrictEqual(xlsx.utils.decode_range('A13:I14'));
+        expect(ranges[5].id).toBe('Bouwjaar');
+    });
+
+    test('triple row range', () => {
+        expect(ranges[2].range)
+            .toStrictEqual(xlsx.utils.decode_range('A6:I8'));
+        expect(ranges[2].id).toBe('Merk en type');
+    });
+
+    test('single row range', () => {
+        expect(ranges[15].range)
+            .toStrictEqual(xlsx.utils.decode_range('A34:I34'));
+        expect(ranges[15].id).toBe('Verzekerde rubrieken');
+    });
+
+    test('end row range', () => {
+        expect(ranges[17].range)
+            .toStrictEqual(xlsx.utils.decode_range('A36:I36'));
+        expect(ranges[17].id).toBe('Verzekerde rubrieken');
+    });
+
+    test('range list borders', () => {
+        expect(ranges.length).toBe(18);
+    });
 });
