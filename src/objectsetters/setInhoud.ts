1147a0330f0a3068e2fcd3677b821faf4f34bb56
Jelte Dirks <j.dirks@outlook.com>
2021-03-01 13:46:10 +0100

    sets inhoud with c / o modifiers

--- src/objectsetters/setInhoud.ts
+++ src/objectsetters/setInhoud.ts
@@ -8,9 +8,26 @@ export function setInhoud(r: RangeID, sheet: Sheet, obj: { [key: string]: any })
     const offset = {r: 0, c: 1};
     const position = {r: r.range.s.r, c: r.range.s.c + offset.c};
     const inhoudCell = sheet[xlsx.utils.encode_cell(position)];
+    const inhoudColumnInt = xlsx.utils.decode_col('B');
+    const weergaveColumnInt = xlsx.utils.decode_col('C');
 
     let inhoud = inhoudCell?.v || '';
 
-    return Object.assign(obj, {inhoud});
+    for (let i = r.range.s.r + 1; i <= r.range.e.r; ++i) {
+        let value = sheet[xlsx.utils.encode_cell({r: i, c: inhoudColumnInt})]?.v;
+        if (!value) continue;
 
+        let weergave = sheet[xlsx.utils.encode_cell({r: i, c: weergaveColumnInt})]?.v;
+        let modifier = ''
+
+        if (weergave.trim() === 'Omschrijving') {
+            modifier = 'o';
+        } else if (weergave.trim() === 'Code') {
+            modifier = 'c';
+        }
+
+        inhoud = inhoud.replace(value, '${' + value + modifier + '}');
+    }
+
+    return Object.assign(obj, {inhoud});
 }
