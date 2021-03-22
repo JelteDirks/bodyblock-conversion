8ffce45f384ee6a0c2525c4d7289df8733c3e7d4
Jelte Dirks <j.dirks@outlook.com>
2021-03-22 09:27:41 +0100

    Sets currency type. All others get 'standaard'.

--- src/objectsetters/setType.ts
+++ src/objectsetters/setType.ts
@@ -3,13 +3,41 @@ import {Sheet} from "xlsx";
 
 const xlsx = require('xlsx');
 
+export interface TypeCheck {
+    re: RegExp;
+    type: string;
+    replace?: (value: string) => string;
+}
+const typeChecks: TypeCheck[] = [
+    {
+        re: /^€\s\d{5}[oc]?$/,
+        type: 'currency',
+        replace: (value: string) => {
+            return value.replace('€', '').trim();
+        }
+    },
+    {
+        re: /.*/,
+        type: 'standaard'
+    }
+];
+
 export function setType(r: RangeID, sheet: Sheet, obj: { [key: string]: any }): { [key: string]: any } {
     const weergaveColumn = xlsx.utils.decode_col('C');
 
     const inhoudColumn = xlsx.utils.decode_col('B');
     const inhoud = sheet[xlsx.utils.encode_cell({c: inhoudColumn, r: r.range.s.r})]?.v;
 
-    console.log(inhoud);
+    for (let i = 0; i < typeChecks.length; i++) {
+        const tc: TypeCheck = typeChecks[i];
+        if (tc.re.test(inhoud)) {
+            Object.assign(obj, {type: tc.type});
+
+            if (!!tc.replace) {
+                Object.assign(obj, {inhoud: tc.replace(inhoud)});
+            }
+        }
+    }
 
     return Object.assign(obj, {});
 }
