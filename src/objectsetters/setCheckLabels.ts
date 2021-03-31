d484780446f0b0fd29697723540da7350f7f6597
Jelte Dirks <j.dirks@outlook.com>
2021-03-31 14:47:22 +0200

    intermittent

--- src/objectsetters/setCheckLabels.ts
+++ src/objectsetters/setCheckLabels.ts
@@ -5,30 +5,32 @@ const xlsx = require('xlsx');
 
 export function setCheckLabels(r: RangeID, sheet: Sheet, obj: { [key: string]: any }): { [key: string]: any } {
 
-    const labelColumn = xlsx.utils.decode_col('B');
-    const verwijderColumn = xlsx.utils.decode_col('E');
-    const weergaveColumn = xlsx.utils.decode_col('C');
-    const checkLabels: string[] = [];
+  const labelColumn = xlsx.utils.decode_col('B');
+  const verwijderColumn = xlsx.utils.decode_col('E');
+  const weergaveColumn = xlsx.utils.decode_col('C');
+  const checkLabels: string[] = [];
 
-    for (let i = r.range.s.r + 1; i <= r.range.e.r; ++i) {
-        let label = sheet[xlsx.utils.encode_cell({r: i, c: labelColumn})]?.v;
-        let verwijderen = sheet[xlsx.utils.encode_cell({r: i, c: verwijderColumn})]?.v;
-        let weergave = sheet[xlsx.utils.encode_cell({r: i, c: weergaveColumn})]?.v;
+  for (let i = r.range.s.r + 1; i <= r.range.e.r; ++i) {
+    let label = sheet[xlsx.utils.encode_cell({r: i, c: labelColumn})]?.v;
+    let verwijderen = sheet[xlsx.utils.encode_cell({r: i, c: verwijderColumn})]?.v;
+    let weergave = sheet[xlsx.utils.encode_cell({r: i, c: weergaveColumn})]?.v;
 
-        let modifier = ''
+    let modifier = ''
 
-        if (weergave?.trim() === 'Omschrijving') {
-            modifier = 'o';
-        } else if (weergave?.trim() === 'Code') {
-            modifier = 'c';
-        }
-
-        if (!label || !verwijderen) continue;
-
-        if (verwijderen.trim().toLowerCase() === 'verwijderen') {
-            checkLabels.push(label + modifier);
-        }
+    if (weergave?.trim() === 'Omschrijving') {
+      modifier = 'o';
+    } else if (weergave?.trim() === 'Code') {
+      modifier = 'c';
     }
 
-    return Object.assign(obj, {checkLabels});
+    if (!label || !verwijderen) continue;
+
+    if (verwijderen.trim().toLowerCase() === 'verwijderen') {
+      checkLabels.push(label + modifier);
+    }
+  }
+  if (checkLabels.length === 0) {
+    return obj;
+  }
+  return Object.assign(obj, {vereiste_labels: checkLabels});
 }
